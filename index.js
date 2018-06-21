b865ac79dec06cca66e5260190f6854a4b7afc37
lilysinger <lilymsinger@gmail.com>
2018-06-21 20:47:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget(){
 }
 
 function increasedRankBy(n){
-  return document.getElementById('app')('ul.rank-list li');
+  return document.getElementById('app').querySelectorAll('ul.rank-list li');
 }
 
 function deepestChild(){
