02c288e96f22aa8e8e4b2f06f3a51bd9b1f216e9
lilysinger <lilymsinger@gmail.com>
2018-06-21 21:09:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 //Code your solution here
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target');
+  
+}
+
+function increaseRankBy(n){
+   const list = document.querySelectorAll('ul.ranked-list')
+  for (let i = 0; i < list.length; i++){
+  list[i].innerHTML = (i + 1).toString()
+}
+}
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+}
